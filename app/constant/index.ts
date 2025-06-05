b0d2656e372305d0f3c31933286fb479907308b9
Arief-alt <145629626+Arief-alt@users.noreply.github.com>
2025-06-05 18:03:41 +0700

    Create A Responsive UI for Todo App
    creating a responsive sidebar

--- app/constant/index.ts
+++ app/constant/index.ts
@@ -1,8 +1,26 @@
 export const sidebarItems = [
     {
         id: 1,
-        icon: "",
-        label: "",
-        href: "",
+        icon: "/assets/icons/home.png",
+        label: "Dashboard",
+        href: "/dashboard",
+    },
+    {
+        id: 2,
+        icon: "/assets/icons/checkbox.png",
+        label: "Completed",
+        href: "/completed",
+    },
+    {
+        id: 3,
+        icon: "/assets/icons/calendar.png",
+        label: "Calendar",
+        href: "/calendar",
+    },
+    {
+        id: 4,
+        icon: "/assets/icons/setting.png",
+        label: "Settings",
+        href: "/settings",
     },
 ];
--- app/routes/root/layout.tsx
+++ app/routes/root/layout.tsx
@@ -1,9 +1,24 @@
 import {Outlet} from 'react-router'
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import { MobileSidebar, NavItems } from 'components';
 
 const Layout = () => {
     return (
-        <div>
-            <Outlet />
+        <div className="admin-layout">
+            <MobileSidebar />
+
+            <aside className="hidden lg:block w-full max-w-[270px]">
+                <SidebarComponent
+                    width={270}
+                    enableGestures={false}
+                >
+                    <NavItems />
+                </SidebarComponent>
+            </aside>
+
+            <aside className="w-full h-full pt-5 lg:pt-10">
+                <Outlet />
+            </aside>
         </div>
     )
 }

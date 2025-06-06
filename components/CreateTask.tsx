f844dee0f19aa292b8f4b52680748d4d70f94730
Arief-alt <145629626+Arief-alt@users.noreply.github.com>
2025-06-06 13:24:21 +0700

    Frontend Done
    Completing Frontend for my todo-app

---
+++ components/CreateTask.tsx
@@ -0,0 +1,106 @@
+import React from "react";
+import { useState } from "react";
+
+const CreateTask = ({onClose}: CreateTaskProps) => {
+    return (
+        <article className="w-102 justify-center items-center">
+            <div className="gap-6 px-6 py-6 flex flex-col bg-black border border-gray-100 shadow-400 rounded-xl">
+                <h1 className="font-semibold text-2xl">
+                    Create New Task
+                </h1>
+
+                <section className="flex flex-col gap-2">
+                    <h1 className="font-semibold font-sm">
+                        Task Title
+                    </h1>
+
+                    <input
+                        type="text"
+                        maxLength={50}
+                        placeholder="Enter task title..."
+                        className="w-full p-2 rounded-lg bg-black border border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-100 cursor-pointer"
+                    />
+                </section>
+
+                <section className="flex flex-col gap-2">
+                    <h1 className="font-semibold font-sm">
+                        Description
+                    </h1>
+
+                    <textarea
+                        placeholder="Add task description..."
+                        maxLength={200}
+                        className="w-full p-2 rounded-lg bg-black border border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-100 cursor-pointer resize-none"
+                        rows={1}
+                        style={{ minHeight: '44px' }}
+                        onInput={(e) => {
+                            const target = e.target as HTMLTextAreaElement;
+                            target.style.height = 'auto';
+                            target.style.height = target.scrollHeight + 'px';
+                        }}
+                    />
+                </section>
+
+                <div className="gap-6 grid grid-cols-2">
+                    <section>
+                        <h1 className="font-semibold font-sm">
+                            Priority
+                        </h1>
+
+                        <img
+                            src="/assets/icons/arrow-down.png"
+                            alt="search"
+                            className="absolute ml-32 pt-3 size-8"
+                        />
+
+                        <select
+                            className="w-full p-2 rounded-lg bg-black border border-gray-600 appearance-none focus:outline-none focus:ring-1 focus:ring-gray-100 cursor-pointer"
+                        >
+                            <option value="High Priority">High Priority</option>
+                            <option value="Medium Priority">Medium Priority</option>
+                            <option value="Low Priority">Low Priority</option>
+                        </select>
+                    </section>
+                    <section>
+                        <h1 className="font-semibold font-sm">
+                            Due Date
+                        </h1>
+
+                        <input
+                            type="text"
+                            maxLength={10}
+                            placeholder="dd/mm/yyyy"
+                            className="w-full p-2 rounded-lg bg-black border border-gray-600 focus:outline-none focus:ring-1 focus:ring-gray-100 cursor-pointer"
+                        />
+                    </section>
+                </div>
+
+                <div className="gap-6 flex items-center justify-end">
+                    <section>
+                        <button
+                            onClick={onClose}
+                            type="submit"
+                            className="flex items-center justify-center gap-2 px-3 py-2 w-32 rounded-lg bg-black cursor-pointer border border-gray-600"
+                        >
+                            <h1>Cancel</h1>
+                        </button>
+                    </section>
+                    <section>
+                        <button
+                            onClick={() => {
+                                console.log("New Task")
+
+                                onClose()
+                            }}
+                            type="submit"
+                            className="flex items-center justify-center gap-2 px-3 py-2 w-32 rounded-lg bg-dark-blue cursor-pointer border border-gray-600"
+                        >
+                            <h1>Create Task</h1>
+                        </button>
+                    </section>
+                </div>
+            </div>
+        </article>
+    )
+}
+export default CreateTask
--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,21 +1,23 @@
-const StatsCard = ({id, name, total, icon}: StatsCard) => {
+const StatsCard = ({id, name, total, icon, color}: StatsCard) => {
     const getBackgroundColor = () => {
         switch(id) {
             case 1:
-                return 'bg-dark-green';
+                return 'bg-light-red border border-red-400';
             case 2:
-                return 'bg-dark-green';
+                return 'bg-dark-green border border-emerald-400';
             case 3:
-                return 'bg-dark-blue';
+                return 'bg-dark-blue border border-blue-400';
+            case 4:
+                return  'bg-light-orange border border-orange-400'
         }
     };
 
     return (
-        <article className={`p-6 flex flex-col gap-6 ${getBackgroundColor()} border border-emerald-800 shadow-400 rounded-xl text-dark-100`}>
+        <article className={`p-6 flex flex-col gap-6 ${getBackgroundColor()} shadow-400 rounded-xl text-dark-100`}>
             <div className="flex flex-row xl:flex-row xl:items-center justify-between">
                 <h1 className="text-gray-400">
                     {name}
-                    <div className="text-gray-300 font-bold text-3xl">
+                    <div className={`${color} font-bold text-3xl`}>
                         {total}
                     </div>
                 </h1>
